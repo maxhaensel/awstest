bf959059e2865c5e088f00315ae5c59bfe021b72
maexdontcha <mh@maxhaensel.de>
2018-11-16 13:41:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+'use strict';
+
+exports.gittest = function(event, context, callback) {
+  var contents = 'gittest'
+  var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'content-type': 'text/html'}
+  };
+  callback(null, result);
+};
 
